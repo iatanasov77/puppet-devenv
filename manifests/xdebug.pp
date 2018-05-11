810f0f5583b6c35d0a55e2c83563a137dcd94b69
Ivan I. Atanasov <i.atanasov77@gmail.com>
2018-05-11 18:38:48 +0300

    Update xdebug.pp

--- manifests/xdebug.pp
+++ manifests/xdebug.pp
@@ -1,4 +1,5 @@
-class devenv::xdebug 
+
+class devenv::xdebug::params
 {
 	case $operatingsystem 
 	{
@@ -27,4 +28,34 @@ class devenv::xdebug
 	$remote_connect_back = '0'
 	$remote_log          = false
 	$idekey              = ''
+}
+
+class devenv::xdebug (
+	$service              = "httpd",
+	$ini_file_path        = $xdebug::params::ini_file_path,
+	$default_enable       = $xdebug::params::default_enable,
+	$remote_enable        = $xdebug::params::remote_enable,
+	$remote_handler       = $xdebug::params::remote_handler,
+	$remote_host          = $xdebug::params::remote_host,
+	$remote_port          = $xdebug::params::remote_port,
+	$remote_autostart     = $xdebug::params::remote_autostart,
+	$remote_connect_back  = $xdebug::params::remote_connect_back,
+	$remote_log           = $xdebug::params::remote_log,
+	$idekey               = $xdebug::params::idekey,
+) inherits devenv::xdebug::params 
+{
+	$zend_extension_module = $xdebug::params::zend_extension_module
+
+	package { "$xdebug::params::package":
+		ensure	=> "installed",
+		require => Package[$xdebug::params::php],
+		notify	=> File[$ini_file_path],
+	}
+
+	file { "$ini_file_path" :
+		content => template('xdebug/ini_file.erb'),
+		ensure  => present,
+		require => Package[$xdebug::params::package],
+		notify  => Service[$service],
+	}
 }
