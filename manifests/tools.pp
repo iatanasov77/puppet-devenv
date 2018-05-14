98a7522cf2bf850ae07a643585c1d366b56efcef
Ivan I. Atanasov <i.atanasov77@gmail.com>
2018-05-14 18:00:29 +0300

    Download and install IAtanasov's bumpversion.php

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -16,4 +16,12 @@ class devenv::tools
 	package { $packages:
 		ensure => present,
 	}
+	
+	wget::fetch { "Download IAtanasov's bumpversion.php":
+		source      => 'https://raw.github.com/iatanasov77/php-dev-tools/develop/bumpversion.php',
+		destination => '/usr/local/bin',
+		timeout     => 0,
+		verbose     => true,
+		mode		=> '0777'
+	}
 }
