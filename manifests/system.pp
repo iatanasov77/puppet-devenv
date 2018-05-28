81517fc00ae53552cd9e823859e13dc948411deb
Ivan I. Atanasov <ia@vankosoft.org>
2018-05-28 12:03:45 +0300

    Restatrt sshd service

--- manifests/system.pp
+++ manifests/system.pp
@@ -6,5 +6,11 @@ class devenv::ssh
 	class { 'ssh':
 		sshd_password_authentication	=> 'yes'
 	}
-
+	
+	# Restatrt sshd service
+	file { '/etc/ssh/sshd_config':
+		ensure  => present,
+		require => Class['ssh'],
+	    notify  => Service['sshd']
+  	}
 }
