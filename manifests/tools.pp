7d30d8d3f7c2df9def1bdeba1bf432aec77acd5a
Ivan I. Atanasov <i.atanasov77@gmail.com>
2018-05-28 08:43:00 +0000

    Commit

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -17,9 +17,20 @@ class devenv::tools
 		ensure => present,
 	}
 	
+	# Install bump version script
 	wget::fetch { "Download IAtanasov's bumpversion.php":
 		source      => 'https://raw.github.com/iatanasov77/php-dev-tools/develop/bumpversion.php',
 		destination => '/usr/local/bin',
+		timeout     => 0,
+		verbose     => true,
+		mode        => '0777',
+		execuser    => 'vagrant'
+	}
+	
+	# Install ftp deploy script
+	wget::fetch { "Download IAtanasov's bumpversion.php":
+		source      => 'https://github.com/iatanasov77/ftp-deployment/releases/download/v2.9/deployment.phar',
+		destination => '/usr/local/bin/deploy',
 		timeout     => 0,
 		verbose     => true,
 		mode        => '0777',
