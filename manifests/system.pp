f5d73b28b2926bee1d388a6fe339b691f537917f
Ivan I. Atanasov <i.atanasov77@gmail.com>
2018-07-13 16:18:34 +0300

    Update system.pp

--- manifests/system.pp
+++ manifests/system.pp
@@ -7,10 +7,4 @@ class devenv::system
 		sshd_password_authentication	=> 'yes'
 	}
 	
-	# Restatrt sshd service
-	file { '/etc/ssh/sshd_config':
-		ensure  => present,
-		require => Class['ssh'],
-	    notify  => Service['sshd']
-  	}
 }
