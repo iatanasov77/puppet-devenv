2855f2aef5a0f8366cc9cf392af65b7768e95930
Ivan I. Atanasov <i.atanasov77@gmail.com>
2019-10-23 17:55:57 +0300

    Update php.pp

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,6 +1,16 @@
 
 class devenv::php
 {
+    # PHP Modules
+    $modules = {}
+    each( $facts['php_modules'] ) |$value| {
+        $modules.merge!({
+            "${value}"  => {
+                ini_prefix => '99-',
+            }
+        })
+    }
+
     class { '::php':
         ensure       => latest,
         manage_repos => true,
@@ -14,46 +24,6 @@ class devenv::php
             'PHP/memory_limit'        => '-1',
             'Date/date.timezone'      => 'Europe/Sofia',
         },
-        extensions => {
-            'intl' => {
-                ini_prefix => '20-',
-            },
-            'gd' => {
-                ini_prefix => '20-',
-            },
-            'mbstring' => {
-                ini_prefix => '20-',
-            },
-            'xmlrpc' => {
-                ini_prefix => '20-',
-            },
-        	'pdo' => {
-	           ini_prefix => '20-',
-	           multifile_settings => true,
-	           settings => {
-	               'pdo'  => {},
-	               'pdo_sqlite' => {},
-	               'sqlite3' => {},
-	            },
-	        },
-	        'mysql' => {
-	           ini_prefix => '20-',
-	           multifile_settings => true,
-	           settings => {
-	               'mysqlnd'  => {},
-	               'mysql' => {},
-	               'mysqli' => {},
-	               'pdo_mysql' => {},
-	               'sysvshm' => {},
-	          },
-	        }
-           
-	        /*
-	        curl    => { },
-	        imagick   => { provider => pecl },
-	        mcrypt    => { provider => pecl },
-	        json    => { provider => pecl },
-	        */
-        }
+        extensions => $modules
     }
 }
