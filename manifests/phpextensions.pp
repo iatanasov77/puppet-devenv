8f55f4d9f20687a329588d44a207414dc4aeee98
Ivan I. Atanasov <i.atanasov77@gmail.com>
2019-10-24 16:24:38 +0300

    Update phpextensions.pp

--- manifests/phpextensions.pp
+++ manifests/phpextensions.pp
@@ -1,6 +1,30 @@
 
 class devenv::phpextensions
 {
+    if ( 'apc' in $facts['php_modules'] )
+    {
+        class { 'devenv::phpapc':
+            config  => {
+                enable_opcode_cache => 1,
+                shm_size            => '512M',
+                stat                => 0
+            }
+        }
+    }
+
+    if ( 'xdebug' in $facts['php_modules'] )
+    {
+        class { 'devenv::xdebug':
+            default_enable       => '1',
+            remote_enable        => '1',
+            remote_handler       => 'dbgp',
+            remote_host          => 'localhost',
+            remote_port          => '9000',
+            remote_autostart     => '1',
+        }
+    }
+    
+    /*
     if ( 'intl' in $facts['devenv_modules'] )
     {
         package { "php${phpVersion}-intl":
@@ -18,27 +42,5 @@ class devenv::phpextensions
             notify  => Service["${apachename}"],
         }
     }
-    
-    if ( 'xdebug' in $facts['devenv_modules'] )
-    {
-        class { 'devenv::xdebug':
-            default_enable       => '1',
-            remote_enable        => '1',
-            remote_handler       => 'dbgp',
-            remote_host          => 'localhost',
-            remote_port          => '9000',
-            remote_autostart     => '1',
-        }
-    }
-    
-    if ( 'apc' in $facts['devenv_modules'] )
-    {
-        class { 'devenv::phpapc':
-            config  => {
-                enable_opcode_cache => 1,
-                shm_size            => '512M',
-                stat                => 0
-            }
-        }
-    }
+    */
 }
