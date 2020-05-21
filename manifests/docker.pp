62263034447e75437351a04db0c24df0c78a01e8
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-05-21 08:21:34 +0000

    Commit

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,13 +1,5 @@
 
 class devenv::docker
 {
-	class { 'docker':
-		ensure => present,
-	  version => 'latest',
-	}
 	
-	class {'docker::compose':
-	  ensure => present,
-	  #version => '1.9.0',
-	}
 }
