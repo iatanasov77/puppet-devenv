fc17dcebf3c993bdb3f8d3a47d1937fb9bd5b6a7
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-05-22 17:03:55 +0000

    Commit

---
+++ manifests/subsystems.pp
@@ -0,0 +1,43 @@
+class devenv::subsystems
+{
+    $vsConfig['subsystems'].each |Integer $index, String $value| {
+     
+        case $value
+        {
+            'phpbrew':
+            {
+                class { 'phpbrew':
+                   system_wide => $vsConfig['phpbrew']['system_wide'],
+                   additional_dependencies => $vsConfig['phpbrew']['additional_dependencies']
+                }
+                
+                $vsConfig['phpbrew']['install'].each |Integer $index, String $value| {
+                    phpbrew::install { $value: 
+                       
+                    }
+                }
+            }
+            'docker':
+            {
+                class { 'docker':
+                    ensure => present,
+                    version => 'latest',
+                }
+                
+                class {'docker::compose':
+                    ensure => present,
+                    #version => '1.9.0',
+                }
+
+            }
+            default:
+            {
+                if ! defined(Package[$value]) {
+                    package { $value:
+                        ensure => present,
+                    }
+                }
+            }
+        }
+    }
+}
