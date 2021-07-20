fa4e8c9d091f967236bbceca60bd64cb3b0ec1ba
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-07-20 13:41:06 +0000

    Fix SubSystem Symfony.

--- manifests/subsystems/symfony.pp
+++ manifests/subsystems/symfony.pp
@@ -2,7 +2,8 @@ class vs_devenv::subsystems::symfony (
 	Hash $config    = {},
 ) {
     Exec { 'Install Symfony CLI':
-      command	=> 'curl -sSL https://get.symfony.com/cli/installer | bash'
+      command		=> 'curl -sSL https://get.symfony.com/cli/installer | bash',
+      environment	=> [ "HOME=/root" ],
     } ->
     Exec { 'Move Symfony CLI globally':
       command	=> 'mv /root/.symfony/bin/symfony /usr/local/bin/symfony'
