c02df6e4e76f3a103a913c2ad79318a85bdee4d1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-09-17 08:51:56 +0000

    Add GitPrompt.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -19,6 +19,24 @@ class vs_devenv::packages (
 					value => $gitUserEmail,
 					user    => 'vagrant',
 				}
+				
+				# Download Git Prompt
+				wget::fetch { "Download GitPrompt Script":
+					source      => "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh",
+					destination => '/usr/local/bin/git-prompt.sh',
+					verbose     => true,
+					mode        => '0777',
+					cache_dir   => '/var/cache/wget',
+				}
+				# Setup Git Prompt
+				file_line { 'source_git_prompt':
+					path => '/home/vagrant/.bashrc',
+					line => 'source /usr/local/bin/git-prompt.sh',
+				} ->
+				file_line { 'use_git_prompt':
+					path => '/home/vagrant/.bashrc',
+					line => "PS1='`if [ $? = 0 ]; then echo "\[\e[32m\] ✔ "; else echo "\[\e[31m\] ✘ "; fi`\[\033[01;32m\]\u@\h\[\033[00m\]:\[\e[01;34m\]\w\[\e[00;34m\] `(( $(git status --porcelain 2>/dev/null | wc -l) == 0 )) && echo "\[\e[01;32m\]" || ( (( $(git status --porcelain --untracked-files=no 2>/dev/null | wc -l) > 0 )) && echo "\[\e[01;31m\]" ) || echo "\[\e[01;33m\]"`$(__git_ps1 "(%s)")`echo "\[\e[00m\]"`\$ '",
+				}
         	}
             'gitflow':
             {
