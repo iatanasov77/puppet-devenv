dabbb5e0820fa204fa79a11708adbee44e7ba2f3
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-10-12 21:08:13 +0000

    Fixing frontendtools.pp with harcoding node versions when installed with NVM.

--- manifests/frontendtools.pp
+++ manifests/frontendtools.pp
@@ -2,24 +2,54 @@ class vs_devenv::frontendtools (
     Hash $frontendtools = {},
 ) {
 
-	if defined( $frontendtools['nvm'] ) {
+	if has_key( $frontendtools, 'nvm' ) {
 		# Multiple NodeJs Versions
 	    class { 'nvm':
-			user => 'vagrant',
+			user 				=> 'vagrant',
+			manage_dependencies	=> false,
+			manage_profile		=> false,
 		} ->
+		file_line { 'add NVM_DIR to profile file':
+	    	path => "/home/vagrant/.bashrc",
+	      	line => "export NVM_DIR=/home/vagrant/.nvm",
+	    } ->
+	    file_line { 'add . ~/.nvm/nvm.sh to profile file':
+	      	path => "/home/vagrant/.bashrc",
+	      	line => "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"  # This loads nvm",
+	    } ->
 		
+		/* This Fails. I dont know why
+		 ================================
 		$frontendtools['nvm'].each |Integer $index, String $nodeVersion| {
+			# Debuging
+			#fail( "NODE INDEX: ${index}		NODE VERSION: ${nodeVersion}" )
+		
+			
 			nvm::node::install { "${nodeVersion}":
-			    user    => 'vagrant',
+			    user    	=> 'vagrant',
 			    set_default => ($index == 0),
-			} ->
+			}
 		}
+		*/
+		
+		nvm::node::install { '16.10.0':
+		    user    	=> 'vagrant',
+		    set_default => true,
+		} ->
+		nvm::node::install { '10.24.1':
+		    user    	=> 'vagrant',
+		    set_default => false,
+		}
+		
+		$requiredPackages	= [ Class['nvm'] ]
 	} else {
 		# Only one NodeJs Version
 	    class { 'nodejs':
 	        version       => "${frontendtools['nodejs']}",
 	        target_dir    => '/usr/bin',
 	    }
+	    
+	    $requiredPackages	= [ Class['nodejs'] ]
 	}
     
     $frontendtools['tools'].each |String $key, Hash $data| {
@@ -39,7 +69,7 @@ class vs_devenv::frontendtools (
             {
             	package { "${key}":
                     provider    => 'npm',
-                    require     => Class['nodejs']
+                    require     => $requiredPackages,
                 }
             }
         }
