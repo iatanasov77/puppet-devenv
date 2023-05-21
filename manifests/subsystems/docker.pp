afd8bb7119e2b73766c64f77dcf08adcda84138b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-21 21:41:22 +0000

    Improve Subsystem Docker.

--- manifests/subsystems/docker.pp
+++ manifests/subsystems/docker.pp
@@ -3,12 +3,17 @@ class vs_devenv::subsystems::docker (
 ) {
 	class { 'docker':
         ensure          => 'present',
-        version         => 'latest',
+        version         => "${config['version']}",
         docker_users    => $config['docker_users'],
     }
     
     class {'docker::compose':
         ensure => present,
-        #version => '1.9.0',
+        version => "${config['composer_version']}",
+    }
+    
+    class { 'vs_devenv::subsystems::docker::portainer':
+        admin_password  => "${config['portainer_password']}",
+        require         => Class['docker'],
     }
 }
