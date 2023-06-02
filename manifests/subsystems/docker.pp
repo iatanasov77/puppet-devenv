67614faccae44c6b791308a75fd56c43ba4e01ca
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-06-02 11:36:15 +0000

    Move Subsystem Docker to the Core Module and Use it From There.

--- manifests/subsystems/docker.pp
+++ manifests/subsystems/docker.pp
@@ -1,19 +1,7 @@
 class vs_devenv::subsystems::docker (
 	Hash $config    = {},
 ) {
-	class { 'docker':
-        ensure          => 'present',
-        version         => "${config['version']}",
-        docker_users    => $config['docker_users'],
-    }
-    
-    class {'docker::compose':
-        ensure => present,
-        version => "${config['composer_version']}",
-    }
-    
-    class { 'vs_devenv::subsystems::docker::portainer':
-        admin_password  => "${config['portainer_password']}",
-        require         => Class['docker'],
+	class { 'vs_core::docker':
+        config  => $config,
     }
 }
