6c2555cc976cdcdcc0b854fe3d47ed88669b4d6b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-08-14 16:57:41 +0000

    Fix Subsystems Namespaces.

--- manifests/subsystems/cloud_platforms.pp
+++ manifests/subsystems/cloud_platforms.pp
@@ -1,4 +1,4 @@
-class vs_devops::subsystems::cloud_platforms (
+class vs_devenv::subsystems::cloud_platforms (
     Hash $config    = {},
 ) {
     class { 'vs_core::cloud_platforms':
--- manifests/subsystems/hashicorp.pp
+++ manifests/subsystems/hashicorp.pp
@@ -1,4 +1,4 @@
-class vs_devops::subsystems::hashicorp (
+class vs_devenv::subsystems::hashicorp (
 	Hash $config       = {},
 ) {
     class { 'vs_core::hashicorp':
