b2d38e94f4a7d6d1c5df559e6ef1fd3f4ae9fdfb
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-30 07:47:22 +0000

    Add NPM Auth Script.

--- manifests/npm_login.pp
+++ manifests/npm_login.pp
@@ -2,10 +2,23 @@ class vs_devenv::npm_login (
     Hash $npmCredentials,
 ) {
 
+    #################################################
     # Set Environement Variables for NPM Access
+    #################################################
     file { "/etc/profile.d/npm_credentials.sh":
         ensure  => present,
         content => template( 'vs_devenv/npm_credentials.sh.erb' )
+    }
+    
+    #######################################
+    # Add NPM Auth Script
+    #######################################
+    file { '/usr/local/bin/npm-auth':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0777',
+        source  => 'puppet:///modules/vs_devenv/npm_auth.sh',
     }
     
 }
