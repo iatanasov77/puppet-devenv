bade85eb10cb4d5d256826a93b788985a33f11ef
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-06-20 05:45:11 +0000

    Improve MS Sql Subsytem.

--- manifests/subsystems/ms_sql.pp
+++ manifests/subsystems/ms_sql.pp
@@ -7,13 +7,10 @@ class vs_devenv::subsystems::ms_sql (
     
     # Create Databases
     $config['databases'].each |String $key, Hash $db| {
-        exec { 'Install mssql-tools':
-            command     => "sqlcmd -S localhost -U SA -P 'Ophthalamia@123' -q \"exit(CREATE DATABASE ${db['name']})\"",
-            require     => [ Class['vs_dotnet::ms_sql::install'] ],
-        } ->
-        exec { 'Install mssql-tools':
-            command     => "sqlcmd -S localhost -U SA -P '${config['rootPassword']}' -q \"exit(RESTORE DATABASE [${db['name']}] FROM DISK='${db['dump']}')\"",
-            require     => [ Class['vs_dotnet::ms_sql::install'] ],
+        vs_dotnet::ms_sql::database { $db['name']:
+            rootPassword    => $config['rootPassword'],
+            dbName          => $db['name'],
+            backupFile      => $db['dump'],
         }
     }
 }
